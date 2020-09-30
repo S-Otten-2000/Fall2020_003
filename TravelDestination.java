3324a4ec437fb1a7e10487081778f65444e155a4
S-Otten-2000 <70922160+S-Otten-2000@users.noreply.github.com>
2020-09-30 12:42:29 -0500

    Update TravelDestination.java

--- TravelDestination.java
+++ TravelDestination.java
@@ -2,7 +2,7 @@
 public class TravelDestination {
 
 	public static void main(String[] args) {
-		System.out.println("My name is Heena Rathore. I wish to travel to England once this Covid goes away!");
+		System.out.println("My name is Stephen Otten. My UTSA ID is Aui924. I like to go on long runs in the afternoon!");
 	}
 
 }
